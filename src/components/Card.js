6ba25017269f795435677431a2a7bacd956e521d
Samalaprashamsa <109023125+Samalaprashamsa@users.noreply.github.com>
2023-11-25 20:31:18 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,19 @@
-// Card.js
+
 import React from 'react';
 import './Card.css';
-import { FaEllipsisH, FaUser } from 'react-icons/fa'; // Import the required icons
+import { FaEllipsisH, FaUser } from 'react-icons/fa'; 
 
 const Card = ({ ticket }) => {
   return (
     <div className="card">
       <div className="id">
         {ticket.id}
-        <FaUser className="profile-icon" /> {/* Add profile photo icon */}
+        <FaUser className="profile-icon" /> 
       </div>
       <div className="content">
         <h3>{ticket.title}</h3>
         <div className="icon-box">
-          <FaEllipsisH className="ellipsis-icon" /> {/* Add horizontal ellipsis icon */}
+          <FaEllipsisH className="ellipsis-icon" /> 
           {ticket.tag && ticket.tag.length > 0 && (
             <div className="tag-box">
               <p className="tag">{`Tag: ${ticket.tag[0]}`}</p>
